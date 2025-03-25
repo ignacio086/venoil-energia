852177c8acf0e51fe89c4de23c2b36ed8a9d3906
Ignacio Gomez <ignaarigomez@gmail.com>
2025-03-25 11:58:44 -0300

    gest

--- src/app/componentes/sections/gestion/Presentacion.tsx
+++ src/app/componentes/sections/gestion/Presentacion.tsx
@@ -13,25 +13,27 @@ export default function Presentacion() {
           <div className="w-full h-full bg-verde/30 rounded-full"></div>
         </div>
       </div>
-      <div className="w-full lg:w-1/2 h-full p-12 flex gap-2 flex-col justify-center text-center items-end lg:items-end">
+      <div className="w-full lg:w-1/2 h-full p-12 flex gap-2 flex-col justify-center text-center items-center lg:items-end">
         <h1 className="text-azul text-xl lg:text-6xl">
           {language == "en" ? "Managment" : "Gestión"}
         </h1>
         <h1 className="text-azul text-xl lg:text-6xl" style={popt.style}>
           {language == "en" ? "Integrated" : "Integrada"}
         </h1>
-        <p className="text-sm text-right lg:text-xl">
+        <p className="text-sm text-center lg:text-right lg:text-xl">
           {language == "en"
             ? "At Venoil Energía, we focus every day on operational excellence, for this, we have an Integrated Management System"
             : "En Venoil Energia, nos enfocamos cada dia en la excelencia operacional, para ello, contamos con un Sistema de Gestión Integrada"}
         </p>
         <div className="flex w-72 h-72 items-center justify-center flex-col gap-2">
-          <div className="w-24 h-24 border-2 border-azul flex items-center justify-center rounded-full">
+          <div className=" h-24 w-12 border-2 border-azul flex items-center justify-center rounded-full">
+          <div className="w-8 h-8 flex items-center justify-center rounded-full">
             <div className="w-6 h-6 bg-verde rounded-full"></div>
           </div>
-          <div className="w-1 h-12 bg-azul"></div>
+          </div>
+          <div className="w-1 h-48 bg-azul"></div>
           <div
-            className={`rounded-xl bg-[url("/isotipo-01.png")] bg-cover aspect-square w-1/2 h-full flex items-end justify-center flex-col bg-no-repeat bg-center`}
+            className={`rounded-xl bg-[url("/isotipo-01.png")] bg-contain aspect-square w-1/2 h-full flex items-end justify-center flex-col bg-no-repeat bg-center`}
           ></div>
         </div>
       </div>
